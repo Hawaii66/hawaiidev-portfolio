63b1acf11032a3b2ab7a726b168d490dce7f91bd
Sebastian <hawaiilive@outlook.com>
2023-11-12 20:24:10 +0100

    fix: lazy load, styling

--- src/components/Knowledge/KnowledgeGraph.tsx
+++ src/components/Knowledge/KnowledgeGraph.tsx
@@ -1,24 +1,65 @@
+"use client";
+
 import { getGraph } from "@/utils/Knowledge/Graph";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Graph from "react-graph-vis";
 
-function KnowledgeGraph() {
-  const nodes: { id: number; label: string; title: string; color: string }[] =
-    getGraph().nodes.map((i, idx) => ({
-      id: idx,
-      label: i.id,
-      title: i.id,
-      color: i.color,
-    }));
-  const links: { from: number; to: number }[] = getGraph().links.map(
-    (link) => ({
-      from: nodes.find((i) => i.label === link.target)?.id ?? 0,
-      to: nodes.find((i) => i.label === link.source)?.id ?? 0,
-    })
-  );
+type Props = {
+  setSelected: (selected: string) => void;
+};
+
+function KnowledgeGraph({ setSelected }: Props) {
+  const [height, setHeight] = useState(200);
+
+  const nodes: {
+    id: number;
+    label: string;
+    title: string;
+    color: string;
+    mass: number;
+  }[] = getGraph().nodes.map((i, idx) => ({
+    id: idx,
+    label: i.id.replace("!", ""),
+    title: i.id,
+    color: i.color,
+    mass: idx === 0 ? 4 : i.id.includes("!") ? 3 : 1,
+  }));
+  const links: {
+    from: number;
+    to: number;
+    color: string;
+    id: number;
+    width: number;
+  }[] = getGraph().links.map((link, idx) => ({
+    from: nodes.find((i) => i.title === link.target)?.id ?? -1,
+    to: nodes.find((i) => i.title === link.source)?.id ?? -1,
+    color: nodes.find((i) => i.title === link.target)?.color ?? "#FFF",
+    id: idx,
+    width: nodes.find((i) => i.title === link.source)?.id === 0 ? 4 : 2,
+  }));
+
+  useEffect(() => {
+    setHeight((document.documentElement.clientHeight ?? 0) * 0.8);
+  }, []);
 
   return (
     <Graph
+      options={{
+        height: `${height}px`,
+        nodes: {
+          borderWidth: 2,
+          borderWidthSelected: 1,
+          margin: 12,
+        },
+      }}
+      events={{
+        selectNode: (node: { nodes: number[] }) => {
+          setSelected(nodes[node.nodes[0]].title.replace("!", ""));
+        },
+        deselectNode: () => {
+          setSelected("");
+        },
+      }}
       graph={{
         nodes: nodes,
         edges: links,
